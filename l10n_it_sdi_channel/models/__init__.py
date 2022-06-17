864bb36cf7b4755e1d0ef92e5ac196ddbbece1bb
Simone Rubino <sir@takobi.online>
2022-06-17 13:00:05 +0200

    [IMP] l10n_it_sdi_channel: Allow user to be notified of new e-bills or e-invoice notifications

--- l10n_it_sdi_channel/models/__init__.py
+++ l10n_it_sdi_channel/models/__init__.py
@@ -1,5 +1,6 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from . import fatturapa_attachment_out
 from . import company
+from . import fatturapa_attachment
+from . import fatturapa_attachment_out
 from . import sdi
